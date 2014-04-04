9058c2c942cf2be0c1425d3764d896f1813169b4
Alex Wood <awood@redhat.com>
2014-04-04 17:08:34 -0400

    Refactor date_modified functionality.
    Rather than monkey patching Jekyll's Page class, use a Liquid
    tag to do the same thing.

--- _plugins/date_modified.rb
+++ _plugins/date_modified.rb
@@ -1,52 +1,63 @@
 #! /usr/bin/env ruby
 =begin
-  Jekyll generator to add the date a source file was last modified in Git.
+  Liquid tag to add the date a source file was last modified in Git.
   If no modification date is found, Jekyll's site.time property is used
-  instead.
+  instead. Follows the same sematics as Liquid's "date" filter (AKA
+  Ruby's strftime).
   Usage:
-    {{ page.date_modified }}
+    {% date_modified format:'%d %B %Y' %}
   Dependency:
     - rugged
 =end
 require 'rugged'
 
 module Jekyll
-  class Page
-    attr_accessor :date_modified
-
-    # Hack to_liquid to expose our date_modified property
-    def to_liquid(attrs = nil)
-      extra_attrs = self.class::ATTRIBUTES_FOR_LIQUID + %w[date_modified]
-      super(attrs || extra_attrs)
+  class DateModifiedTag < Liquid::Tag
+    def initialize(tag_name, text, tokens)
+      @tag_name = tag_name
+      @attributes = {}
+      text.scan(Liquid::TagAttributes) do |k, v|
+        @attributes[k] = v
+      end
+      @tokens = tokens
+      super
     end
-  end
-
-  class DateModifiedGenerator < Generator
-    safe true
-    priority :normal
 
     def logger
       Jekyll.logger
     end
 
-    def generate(site)
-      $stderr.puts # Places newline after "Generating..."
+    def render(context)
+      site = context.registers[:site]
+      page = context.registers[:page]
       source_root = site.source
       begin
         repo = Rugged::Repository.new(File.join(source_root, ".git"))
         index = repo.index
-        site.pages.each do |page|
-          obj = index[page.path]
-          logger.warn("Warning:", "#{page.path} not found in Git index. Using current time.") unless obj
-          mtime = obj.nil? ? nil : obj[:mtime]
-          page.date_modified = mtime || site.time
+        obj = index[page['path']]
+        if obj.nil?
+          logger.warn("Warning:", "#{page['path']} not found in Git index. Using current time.")
+        else
+          mtime = obj[:mtime]
         end
       rescue Rugged::RepositoryError
         logger.error("Error:", "Missing Git repository! Defaulting to current time.")
-        site.pages.each do |page|
-          page.date_modified = site.time
-        end
       end
+      # Set mtime to the site's generation time if we hit a RepositoryError or nil obj
+      mtime ||= site.time
+
+      format = @attributes['format']
+      # Delegate to Liquid's date filter
+      # Basically, we're doing the below but more efficiently. The
+      # context[format] will resolve the format value within the context (which
+      # we'd want to do if the user has a variable set with the format in it
+      # for example)
+      #
+      # template = Liquid::Template.parse("{{ mtime | date: #{format} }}")
+      # template.render!({"mtime" => mtime})
+      context.invoke('date', mtime, context[format])
     end
   end
 end
+
+Liquid::Template.register_tag('date_modified', Jekyll::DateModifiedTag)
