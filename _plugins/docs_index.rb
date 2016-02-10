4ce0ade1bac0411ee544a81f1ddfed68cafde2c9
Alex Wood <awood@redhat.com>
2016-02-10 16:17:42 -0500

    Hide a few pages from the index functionality.
    Move site to 2-column layout with index on left.
    
    - Move site index to _data directory.
    - Alter index creation tag so it only generates the index data structure
      once.
    - Add JS to keep the side-column stretched the height of the content
      column.
    - Add a page listing uncategorized pages (and hide it from the index).
    
    Highlight the node the user is on in the left-hand nav.

--- _plugins/docs_index.rb
+++ _plugins/docs_index.rb
@@ -6,7 +6,7 @@
 require_relative 'mixins.rb'
 
 module Jekyll
-  class TocItem < Struct.new(:level, :number, :name, :link,:has_sublevels)
+  class TocItem < Struct.new(:level, :number, :name, :link, :has_sublevels)
     include Liquify
   end
 
@@ -14,7 +14,93 @@ module Jekyll
     include Liquify
   end
 
-  class DocsIndexTag < Liquid::Tag
+  class IndexGenerator < Generator
+    include LogCapable
+
+    # Adds titles of pages to Master Table of Contents.
+    # When a page info is extracted from project_pages hash,
+    # the page is removed from project_pages. This way we can track
+    # uncategorized pages
+    def add_titles_and_subsections(sections, project_pages)
+      sections.each do |s|
+        section_name = s['section']
+        page = project_pages.delete(section_name)
+        if page.nil?
+          logger.abort_with("FATAL:", "Page '#{section_name}.md' that is defined in Master Table of Contents wasn't found!")
+        end
+
+        s['section-title'] = page[:title]
+        s['subs'] ||= []
+
+        # Each sub is an existing page, add title to each one
+        add_titles_and_subsections(s['subs'], project_pages)
+      end
+    end
+
+    # Takes enriched sections yaml and creates numbered hierarchy
+    # of sections
+    def build_toc_entries(sections, level, number)
+      section_num = 1
+      result = []
+      sections.each do |s|
+        if number == ""
+          secnum = section_num.to_s
+        else
+          secnum = "#{number}.#{section_num.to_s}"
+        end
+        # This is a little brittle.  We're using the value specified in the YAML file to build the href in the
+        # anchor tag, but the values in the YAML files don't have any directory information associated with them.
+        # I would prefer to match the s['section'] values to actual Page objects and then use page.url
+        result << TocItem.new(level, secnum, s['section-title'], s['section'], s.key?('subs') && s['subs'].length > 0)
+        if s.key?('subs')
+          result.concat(build_toc_entries(s['subs'], level + 1, secnum))
+        end
+        section_num = section_num + 1;
+      end
+
+      result
+    end
+
+    # Retrieve few simple pieces of information for pages on site.
+    #
+    # Returns a hash indexed by page name. Each element of the hash
+    # contains title (:title)
+    def pages_by_name(site)
+      result = {}
+      site.pages.each do |p|
+        # Hash key is page name without .md or .html suffix
+        clean_name = p.name.gsub(/(\.html$)|(\.md$)/,'')
+        next if p.data['toc_display'] == false
+        result[clean_name] = { :title => p.data['title'] || p.name }
+      end
+      result
+    end
+
+    INDEX_KEY = "toc".freeze
+
+    def generate(site)
+      # Load _data/toc.yaml
+      sections_yaml = site.data[INDEX_KEY]
+      all_pages = pages_by_name(site)
+
+      # After this method is called, all unused pages will stay in all_pages
+      add_titles_and_subsections(sections_yaml, all_pages)
+      toc_entries = build_toc_entries(sections_yaml, 1, "")
+
+      uncat=[]
+      all_pages.each do |k, v|
+        logger.warn("Detected uncategorized page '#{k}'. Please categorize pages in _data/#{INDEX_KEY}.yaml")
+        uncat << UncatPage.new(v[:title], k)
+      end
+
+      site.data[INDEX_KEY] = {
+        'entries' => toc_entries,
+        'uncategorized' => uncat
+      }
+    end
+  end
+
+  class IndexTag < Liquid::Tag
     attr_accessor :tag_name
     attr_accessor :text
     attr_accessor :tokens
@@ -24,97 +110,19 @@ module Jekyll
     def initialize(tag_name, text, tokens)
       super
       @tag_name = tag_name
-      @text = text
+      @text = text.strip
       @tokens = tokens
-    end
-
-    # Adds titles of pages to Master Table of Contents.
-    # When a page info is extracted from project_pages hash,
-    # the page is removed from project_pages. This way we can track
-    # uncategorized pages
-    def self.add_titles_and_subsections(sections, project_pages)
-      sections.each do |s|
-        section_name = s['section']
-        page = project_pages.delete(section_name)
-        if page.nil?
-          raise "Page '#{section_name}.md' that is defined in Master Table of Contents wasn't found!"
-        end
-        s['section-title'] = page[:title]
-
-        s['subs'] ||= []
-
-        # Each sub is an existing page, add title to each one
-        DocsIndexTag.add_titles_and_subsections(s['subs'], project_pages)
-      end
-    end
-
-    # Takes enriched sections yaml and creates numbered hierarchy
-    # of sections
-    def self.build_toc_items(sections, level, number)
-      section_num = 1
-      result = []
-      sections.each do |s|
-        if number == ""
-          secnum = section_num.to_s
-        else
-          secnum = "#{number}.#{section_num.to_s}"
-        end
-        result << TocItem.new(level, secnum, s['section-title'], s['section'], s.key?('subs') && s['subs'].length > 0)
-        if s.key?('subs')
-          result.concat(DocsIndexTag.build_toc_items(s['subs'], level + 1, secnum))
-        end
-        section_num = section_num + 1;
-      end
-
-      return result
-    end
-
-    # Retrieve few simple pieces of information for pages on site.
-    # Excludes this_page.
-    #
-    # Returns a hash indexed by page name. Each element of the hash
-    # contains title (:title)
-    def self.pages_by_name(site, this_page)
-      result = {}
-      project_pages = site.pages.select do |p|
-        File.dirname(p.path) == File.dirname(this_page['path']) && p.url != this_page['url']
-      end
-      project_pages.each do |p|
-        # Hash key is page name without .md or .html suffix
-        clean_name = p.name.gsub(/(\.html$)|(\.md$)/,'')
-        result[clean_name] = { :title => p.data['title'] }
-      end
-      return result
     end
 
     def render(context)
       site = context.registers[:site]
       page = context.registers[:page]
-
-      layout = site.layouts[tag_name]
-
-      sections_yaml = page['sections']
-
-      all_pages = DocsIndexTag.pages_by_name(site, page)
-
-      # After this method is called, all unused pages will stay in all_pages
-      DocsIndexTag.add_titles_and_subsections(sections_yaml, all_pages)
-      tocitems = DocsIndexTag.build_toc_items(sections_yaml, 1, "")
-
-      uncat=[]
-      
-      all_pages.each do |k, v|
-        logger.warn("Detected uncategorized page '#{k}'. Please categorize all your pages in index.md")
-        uncat << UncatPage.new(v[:title], k)
-      end
-      
-      #Signal to the template, so that it can include necessary Javascript
-      page['mastertoc'] = true
-      payload = Utils.deep_merge_hashes(site.site_payload, { 'tocitems' => tocitems, 'uncategorized' => uncat})
-
+      logger.abort_with("FATAL:", "Could not find layout named '#{@text}'") unless site.layouts.key?(@text)
+      layout = site.layouts[@text]
+      payload = Utils.deep_merge_hashes(site.site_payload, { "page" => page })
       Liquid::Template.parse(layout.content).render!(payload)
     end
   end
 end
 
-Liquid::Template.register_tag('docs_index', Jekyll::DocsIndexTag)
+Liquid::Template.register_tag('index', Jekyll::IndexTag)
---
+++ docs/candlepin/uncategorized.md
@@ -0,0 +1,9 @@
+---
+title: Uncategorized Pages
+toc_display: false
+---
+# Uncategorized Pages
+
+The following pages need to be categorized:
+
+{% index uncategorized_index %}
