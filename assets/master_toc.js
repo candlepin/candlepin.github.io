ee5e13dbb2f0f263537029087d5a0d8fd3f49478
Alex Wood <awood@redhat.com>
2016-02-11 14:44:21 -0500

    Highlight the node the user is on in the left-hand nav.

--- assets/master_toc.js
+++ assets/master_toc.js
@@ -27,9 +27,11 @@ var glyph = "glyphicon";
 var default_level_collapse = 2;
 
 $(document).ready(function() {
-  var collapsed_items = $("li[data-level]").filter(function() {
+  var collapsed_items = $('li[data-level]').filter(function() {
+    // If this node is highlighted or if its sibling is highlighted, we need to show this node.
+    var should_be_expanded = $(this).hasClass('highlight-node') || $(this).siblings('.highlight-node').length !== 0;
     // Close all subsections that are on level > default_level_collapse
-    return $(this).attr("data-level") > default_level_collapse;
+    return $(this).attr('data-level') > default_level_collapse && !should_be_expanded;
   });
 
   // 0 means slide up quickly :-)
@@ -40,8 +42,15 @@ $(document).ready(function() {
   $("span.hide_link").addClass(minus);
 
   // Set all hide links on levels below default_level_collapse to plus
-  var collapsed_items_with_children = $("li[data-level = '" + default_level_collapse + "'] > span.hide_link");
-  collapsed_items_with_children.removeClass(minus).addClass(plus);
+  var expandable_items =
+    $("li[data-level = '" + default_level_collapse + "'] > span.hide_link").filter(function(i, e) {
+      // Due to the way the Jekyll template constructs the DOM, each sub-section is actually an unordered-list that
+      // immediately follows the logical parent.
+      return !($(e).parent().next().children('.highlight-node').length !== 0);
+    });
+  // Special exemption for a highlighted node that is actually a logical parent.  Always set this to a plus
+  expandable_items = expandable_items.add($('li[data-level]').filter('li.highlight-node').children('span.hide_link'));
+  expandable_items.removeClass(minus).addClass(plus);
 
   // Resize the sidebar
   if ($('.index-column').length > 0) {
@@ -50,15 +59,23 @@ $(document).ready(function() {
 
   $(".hide_link").click(function(event) {
       // Class and level of wrapper li.
-      var this_level_class = $(this).parent().attr('class');
-      var this_data_level = $(this).parent().attr('data-level');
+      var this_parent = $(this).parent();
+
+      // Note that this call to filter is using Javascript's Array filter function and not
+      // JQuery's Element filter function.  Conveniently, the argument order for the two
+      // is reversed.  Array is function(element, index) while Element is
+      // function(index, element).  Thanks Javascript!
+      var this_level_class = this_parent.attr('class').split(/\s+/).filter(function(e) {
+        return e !== "highlight-node";
+      }).join(' ');
+      var this_data_level = this_parent.attr('data-level');
 
       var sublevels = $("li[class*='" + this_level_class + ".']");
       var sublevels_with_children = $("li[class*='" + this_level_class + ".'] > span.hide_link");
 
       var collapsed_sublevel_items = sublevels.filter(function(i, e) {
         // Close all subsections that are more than 1 level down
-        return $(e).attr("data-level") > (parseInt(this_data_level, 10) + 1);
+        return $(e).attr('data-level') > (parseInt(this_data_level, 10) + 1);
       });
 
       if ($(this).hasClass(plus)) {
